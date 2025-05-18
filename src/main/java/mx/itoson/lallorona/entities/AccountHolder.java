0aa992a03f5db868ed65cf59fcd88d392b2dd6a1
Vanni <Vanni@DESKTOP-L20BQP3>
2025-05-17 19:07:39 -0700

    Get and set

--- src/main/java/mx/itoson/lallorona/entities/AccountHolder.java
+++ src/main/java/mx/itoson/lallorona/entities/AccountHolder.java
@@ -4,15 +4,102 @@
  */
 package mx.itoson.lallorona.entities;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  *
  * @author Vanni
  */
 class AccountHolder {
+
+    /**
+     * @return the code
+     */
+    public int getCode() {
+        return code;
+    }
+
+    /**
+     * @param code the code to set
+     */
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the city
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * @param city the city to set
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * @return the taxpayerId
+     */
+    public String getTaxpayerId() {
+        return taxpayerId;
+    }
+
+    /**
+     * @param taxpayerId the taxpayerId to set
+     */
+    public void setTaxpayerId(String taxpayerId) {
+        this.taxpayerId = taxpayerId;
+    }
+
+    /**
+     * @return the zipCode
+     */
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    /**
+     * @param zipCode the zipCode to set
+     */
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
     private int code;
     private String name;
     private String address;
     private String city;
+    @SerializedName("taxpayer-id")
     private String taxpayerId;
     private int zipCode;
     
--- src/main/java/mx/itoson/lallorona/entities/Transactions.java
+++ src/main/java/mx/itoson/lallorona/entities/Transactions.java
@@ -12,6 +12,76 @@ import mx.itoson.lallorona.enums.TransactionType;
  * @author Vanny
  */
 public class Transactions {
+
+    /**
+     * @return the date
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the reference
+     */
+    public String getReference() {
+        return reference;
+    }
+
+    /**
+     * @param reference the reference to set
+     */
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    /**
+     * @return the amount
+     */
+    public float getAmount() {
+        return amount;
+    }
+
+    /**
+     * @param amount the amount to set
+     */
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    /**
+     * @return the type
+     */
+    public TransactionType getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
     
    private Date date;
    private String description;
